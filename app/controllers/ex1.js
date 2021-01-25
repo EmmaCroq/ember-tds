fe701a30840cd703b62f8f381e5199bc341139e5
Emma Croq <21902471@etu.unicaen.fr>
2021-01-25 11:05:14 +0100

    creation d'un component ex1-ajout des imports-definition des membres

---
+++ app/controllers/ex1.js
@@ -0,0 +1,22 @@
+import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+
+export default class Ex1Controller extends Controller {
+  MAX = 100;
+  @tracked content;
+  @tracked info;
+
+  @action
+  clear() {
+    this.clear();
+  }
+  @action
+  save() {
+    this.save();
+  }
+  @action
+  update() {
+    this.update();
+  }
+}
