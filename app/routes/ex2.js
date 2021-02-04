6681189de6fe8e458f03ed279198a53adec31097
Emma Croq <21902471@etu.unicaen.fr>
2021-02-04 15:12:23 +0100

    feat(ex2): add design, message, list and button

--- app/routes/ex2.js
+++ app/routes/ex2.js
@@ -1,4 +1,14 @@
 import Route from '@ember/routing/route';
+import { datas } from '../data/data';
+import Services from '../classes/services';
+import { action, set } from '@ember/object';
 
 export default class Ex2Route extends Route {
+  model() {
+    return new Services(datas);
+  }
+
+  @action toggleActive(s) {
+    set(s, 'active', !s.active);
+  }
 }
