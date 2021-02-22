ef56c86a40b507e0cdd79cb1d0f1ca08e7ab05ef
Emma Croq <21902471@etu.unicaen.fr>
2021-02-22 16:38:20 +0100

    feat(categories): add hbs and js categories with issues on BsNav

--- app/routes/categories.js
+++ app/routes/categories.js
@@ -1,4 +1,13 @@
 import Route from '@ember/routing/route';
 
 export default class CategoriesRoute extends Route {
+  model() {
+    return this.store.findAll('category');
+  }
+
+  //debugger;
+  afterModel(model) {
+    this.store.findAll('contact');
+    this.transitionTo('categories');
+  }
 }
