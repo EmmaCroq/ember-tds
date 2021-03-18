6d740e8979a50d8b4124a5ea9f698f10f7a3d0cc
Emma Croq <21902471@etu.unicaen.fr>
2021-03-18 13:35:55 +0100

    feat(logout): add logout route and function

--- app/routes/board.js
+++ app/routes/board.js
@@ -1,7 +1,12 @@
 import Abstractroute from './abstractroute';
+import { action } from '@ember/object';
 
 export default class BoardRoute extends Abstractroute {
   model() {
     return this.userAuth.getUser();
+  }
+
+  @action logout() {
+    this.transitionTo('logout');
   }
 }
