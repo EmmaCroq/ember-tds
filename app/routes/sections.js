3e612bf9d52a1e51b4cc9786ebb775f84b4a5ef3
Emma Croq <21902471@etu.unicaen.fr>
2021-03-20 22:23:14 +0100

    fix/feat(product): fix edit and add product page

--- app/routes/sections.js
+++ app/routes/sections.js
@@ -1,7 +1,11 @@
 import Abstractroute from './abstractroute';
+import { action } from '@ember/object';
 
 export default class SectionsRoute extends Abstractroute {
   model() {
-    return this.store.findAll('section');
+    return this.store.findAll('section', { include: 'products' });
+  }
+  @action delete(section) {
+    section.deleteRecord();
   }
 }
--- app/routes/sections/add-product.js
+++ app/routes/sections/add-product.js
@@ -1,4 +1,7 @@
 import Route from '@ember/routing/route';
 
 export default class SectionsAddProductRoute extends Route {
+  model() {
+    return this.store.findAll('product');
+  }
 }
