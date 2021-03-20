a9fe0bfb0a69b825509ac39952d575995cc5217c
Emma Croq <21902471@etu.unicaen.fr>
2021-03-20 18:10:42 +0100

    feat(edit): add edit page functional

--- app/routes/sections/edit.js
+++ app/routes/sections/edit.js
@@ -1,4 +1,13 @@
 import Route from '@ember/routing/route';
+import { action } from '@ember/object';
 
 export default class SectionsEditRoute extends Route {
+  model(params) {
+    return this.store.findRecord('section', params.section_id);
+  }
+
+  @action editSection(section) {
+    //TO DO
+    section.save().then(() => this.transitionTo('sections'));
+  }
 }
